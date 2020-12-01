b7e465bb00e12b5bf50ea08aa1ebed1d1808f163
liuguoqing <825994808@qq.com>
2020-12-01 15:06:46 +0800

    添加

--- Netty-code/src/main/java/com/adb/netty/protocol/NettyProtocolServerHandler.java
+++ Netty-code/src/main/java/com/adb/netty/protocol/NettyProtocolServerHandler.java
@@ -4,8 +4,28 @@ import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
+import java.net.SocketAddress;
+
 public class
 NettyProtocolServerHandler extends SimpleChannelInboundHandler<MessageProtocolDto>{
+
+    /**
+     * 客户端连接就会调用
+     * @param ctx
+     * @throws Exception
+     */
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        SocketAddress socketAddress = ctx.channel().remoteAddress();
+        System.out.println("客户端地址为---"+socketAddress.toString());
+    }
+
+    /**
+     * 读取通道数据
+     * @param ctx
+     * @param msg
+     * @throws Exception
+     */
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, MessageProtocolDto msg) throws Exception {
         System.out.println("服务端收到的数据为---"+msg.toString());
@@ -18,6 +38,12 @@ NettyProtocolServerHandler extends SimpleChannelInboundHandler<MessageProtocolDt
         channel.writeAndFlush(dto);
     }
 
+    /**
+     * 出现异常后关闭
+     * @param ctx
+     * @param cause
+     * @throws Exception
+     */
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
         Channel channel = ctx.channel();
